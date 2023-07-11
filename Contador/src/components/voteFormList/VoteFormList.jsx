0806aa2f2352b9007ddaecd015e2c17c92a2eced
DopinARG <duchiarg@gmail.com>
2023-07-10 22:11:56 -0300

    input

--- Contador/src/components/voteFormList/VoteFormList.jsx
+++ Contador/src/components/voteFormList/VoteFormList.jsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import {Form, Button,Col, Row } from 'react-bootstrap'
+import {Form, Button,Col, Row , InputGroup} from 'react-bootstrap'
 import configJson from '../../utils/config.json'
 import axios from 'axios'
+
 
 function VoteFormList() {
     const [localidad, setLocalidad] = useState([]);
@@ -20,10 +21,24 @@ function VoteFormList() {
         try{
         const response = await axios.get(url);
         setLocalidad(response.data.data.localidades);
+        console.log(response);
         } catch(err){
             console.log(err)
         }
     }
+    const handleVotosPresidente=(e) => {
+      setPresidente(e.target.value) 
+
+    }
+    const handleVotosGobernador=(e) => {
+      setGobernador(e.target.value) 
+
+    }
+    const handleVotosIntendentee=(e) => {
+      setIntendente(e.target.value) 
+
+    }
+
     const getEscuelas = async (id) => {
         var url = configJson.backend_url + 'escuelas/' + id;
         try{
@@ -56,7 +71,11 @@ function VoteFormList() {
         const form = {
             id_localidad: localidadSeleccionada,
             id_escuela: escuelaSeleccionada,
-            nro_mesa: mesaSeleccionada
+            nro_mesa: mesaSeleccionada,
+            votosPresidente: presidente,
+            votosGobernador: gobernador,
+            votosIntendente: intendente
+
         }
 
         // preparar formulario según documentación para realizar llamado a POST /votos/
@@ -68,7 +87,7 @@ function VoteFormList() {
     }, []);
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
     <Row>
       <Col lg={4} xs={12}>
         <Form.Group controlId="selector1">
@@ -84,7 +103,7 @@ function VoteFormList() {
         <Form.Group controlId="selector2">
           <Form.Label>Escuela</Form.Label>
           <Form.Control as="select" onChange={handleSelectEscuela}>
-            <option disabled selected value="">
+            <option disabled  value="">
                 Seleccionar escuela
             </option>
             {escuela.map(escuela => (
@@ -97,7 +116,7 @@ function VoteFormList() {
         <Form.Group controlId="selector3">
           <Form.Label>Mesa</Form.Label>
           <Form.Control as="select" onChange={handleSelectMesa}>
-            <option disabled selected value="">
+            <option disabled  value="">
                 Seleccionar mesa
             </option>
             {mesa.map(mesa => (
@@ -107,9 +126,108 @@ function VoteFormList() {
         </Form.Group>
       </Col>
     </Row>
+    <br></br>
+    <Row>
+      <Col lg={4} xs={12}>          {/*una opcion para colocar los nombres de cada candidato */}
+    <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">nombre del candidato</InputGroup.Text>
+        <Form.Control
+          placeholder="nombre del candidato"
+          aria-label="nombre del candidato"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+
+
+      </Col>
+      
+    </Row>
+
+      <Row>
+        <h1>Unión por la Patria</h1>
+        <h2>Lista Celeste y Blanca: Sergio Massa - Agustín Rossi</h2>
+
+      </Row>
               {/* Inputs de candidatos */}
       
     <Row>
+    <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>presidente nombre</Form.Label>
+          <Form.Control  onChange={handleVotosPresidente} as="input" value={presidente} placeholder='Candidato1' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+
+      <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>Gobernador nombre</Form.Label>
+          <Form.Control  onChange={handleVotosGobernador} as="input" value={gobernador} placeholder='gobernador' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+
+      <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>Intendente nombre</Form.Label>
+          <Form.Control  onChange={handleVotosIntendentee} as="input" value={intendente} placeholder='intendente' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+      
+
+    </Row>
+    <Row>
+        <h1>Juntos por el Cambio</h1>
+        <h2>Lista El Cambio de Nuestras Vidas: Horacio Rodríguez Larreta - Gerardo Morales</h2>
+
+      </Row>
+    <Row>
+    <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>presidente nombre</Form.Label>
+          <Form.Control  onChange={handleVotosPresidente} as="input" value={presidente} placeholder='Candidato1' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+
+      <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>Gobernador nombre</Form.Label>
+          <Form.Control  onChange={handleVotosGobernador} as="input" value={gobernador} placeholder='gobernador' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+
+      <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>Intendente nombre</Form.Label>
+          <Form.Control  onChange={handleVotosIntendentee} as="input" value={intendente} placeholder='intendente' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+      
+
+    </Row>
+    <Row>
+      <h2>Lista La Fuerza del Cambio: Patricia Bullrich - Luis Petri </h2>
+    </Row>
+    <Row>
+    <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>presidente nombre</Form.Label>
+          <Form.Control  onChange={handleVotosPresidente} as="input" value={presidente} placeholder='Candidato1' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+
+      <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>Gobernador nombre</Form.Label>
+          <Form.Control  onChange={handleVotosGobernador} as="input" value={gobernador} placeholder='gobernador' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+
+      <Col lg={4} xs={12}>
+        <Form.Group >
+          <Form.Label>Intendente nombre</Form.Label>
+          <Form.Control  onChange={handleVotosIntendentee} as="input" value={intendente} placeholder='intendente' type='number' min={0}/>
+        </Form.Group>
+      </Col>
+      
 
     </Row>
     <Row style={{marginTop: "20px"}}>
